641059ce6f4bc805daea31a94a6e51b125d44cdd
Hugo <h.palomerabecerra@gmail.com>
2022-05-13 14:15:01 -0600

    created app

---
+++ client/app.js
@@ -0,0 +1,19 @@
+
+
+
+const socket = io();
+
+
+$("form").on("submit", function () {
+  let text = $("#name").val();
+  let text2 = $("#input2").val();
+  let text3 =`${text} says: ${text2}`;
+  socket.emit("message", text3);
+  $("#name").val();
+  $("#input2").val();
+  return false;
+});
+ /// display
+socket.on("message", function (text3) {
+  $("<li>").text(text3).appendTo("#history");
+});
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const http = require("http");
+const server = http.Server(app);
+
+app.use(express.static("client"));
+
+const io = require("socket.io")(server);
+
+io.on("connection", function (socket) {
+  socket.on("message", function (text3) {
+    io.emit("id", text3);
+  });
+});
+
+
+server.listen(8080, function () {
+  console.log("Chat server running on PORT:8080");
+});
