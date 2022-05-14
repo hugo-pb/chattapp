fd697298340a6e939ea043d5a6349fee3665f9b2
Hugo <h.palomerabecerra@gmail.com>
2022-05-13 23:01:40 -0600

    added time and style

--- client/app.js
+++ client/app.js
@@ -1,9 +1,14 @@
 const socket = io();
+const time = new Date();
+
 
 $("form").on("submit", function () {
+
+
+
   let text = $("#name").val();
   let text2 = $("#input2").val();
-  let text3 = `${text} says: ${text2}`;
+  let text3 = `${text} says: ${text2} (sended at ${time.getHours()}:${ time.getMinutes()})`;
   socket.emit("message", text3);
   $("#name").val();
   $("#input2").val();
